820cff806a6bf0fe8caccb8ad681d5c5d98d4d47
Colby Hemond <hemond.colby@gmail.com>
2022-06-10 19:46:27 -0400

    adjust styling

--- components/PostTitle.js
+++ components/PostTitle.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
     return (
-      <h1 className="mt-20 text-3xl text-center">
+      <h1 className="mt-20 text-3xl text-center md:text-6xl max-w-[700px]">
         {children}
       </h1>
     )
