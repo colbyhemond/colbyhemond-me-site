49a5712b4a72e4e4d3f2fe15ddefd846e8faf9a9
Colby Hemond <hemond.colby@gmail.com>
2022-06-15 20:40:51 -0400

    add google analytics (#7)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+
   return (
     <Layout>
       <Component {...pageProps} />
