9f172dc8c41cd0d5ff72f5ccc1467136d2cb72c7
Colby Hemond <hemond.colby@gmail.com>
2022-11-29 09:00:39 -0500

    update

--- components/OpenGraphImage.js
+++ components/OpenGraphImage.js
@@ -1,20 +1,15 @@
 import { CldOgImage } from 'next-cloudinary'
 
 const OpenGraphImage = ({ title }) => {
-    return (<>
-        <CldOgImage 
-            src="images/white"
-            overlays={[{
-                text: {
-                    text: title.toUpperCase(),
-                    fontSize: 100,
-                    fontFamily: "Montserrat",
-                    width: '90%',
-                    height: '90%'
-                }
-            }]}/>
-        
-    </>)
+
+    const convertToUrl = (text) => {
+        text.replace(' ', '%20')
+
+    }
+
+    let text = convertToUrl(title).toUpperCase()
+
+    return (`https://res.cloudinary.com/dhhijcjym/image/upload/c_fill,w_2400,h_1200,g_center/w_2000,c_fit,l_text:Montserrat_100_bold:${text},/fl_layer_apply,fl_no_overflow/f_auto/q_auto/v1/images/white`)
 }
 
 export default OpenGraphImage
