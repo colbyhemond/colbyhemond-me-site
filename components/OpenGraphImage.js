5bce4230dcb8ed49e54d9b4a9f5438b9849c4834
Colby Hemond <hemond.colby@gmail.com>
2022-11-29 08:01:02 -0500

    update og image

--- components/OpenGraphImage.js
+++ components/OpenGraphImage.js
@@ -2,29 +2,7 @@ import { CldOgImage } from 'next-cloudinary'
 
 const OpenGraphImage = ({ title }) => {
     return (<>
-        <CldOgImage
-  src="images/white"
-  overlays={[
-    {
-      width: 1024,
-      crop: 'fit',
-      text: {
-        color: 'black',
-        fontFamily: 'Source Sans Pro',
-        fontSize: 150,
-        fontWeight: 'bold',
-        text: title
-      },
-      position: {
-        x: 100,
-        y: -100,
-        gravity: 'west',
-      },
-    }
-  ]}
-  twitterTitle={title}
-  alt={`White background with text ${title}`}
-/>
+        <CldOgImage text={title}/>
         
     </>)
 }
