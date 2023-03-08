bddfa926772eda060fb53a79ceeb34e14310dcd6
Colby Hemond <hemond.colby@gmail.com>
2023-03-08 05:16:56 -0500

    update og img (#14)

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -10,7 +10,7 @@ import OpenGraphImage from "../../components/OpenGraphImage"
 export default function Post({post}) {
 
 let text = encodeURIComponent(post.title).toUpperCase()
-let ogUrl = `https://res.cloudinary.com/dhhijcjym/image/upload/c_fill,w_2400,h_1200,g_center/w_2000,c_fit,l_text:Montserrat_100_bold:${text},/fl_layer_apply,fl_no_overflow/f_auto/q_auto/v1/images/white`
+let ogUrl = `https://res.cloudinary.com/dhhijcjym/image/upload/c_fill,w_2400,h_1200,g_center/w_2000,c_fit,l_text:Montserrat_100_bold:${text},/fl_layer_apply,fl_no_overflow/f_auto/q_auto/v1/images/colbyhemond-og-image-1`
 
   return (
     <>
